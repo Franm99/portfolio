3f4122371eec5db744fef8f17c79b07d06815f55
Fran Moreno <fran.moreno.se@gmail.com>
2023-10-11 10:24:57 +0200

    Added Railway domain to allowed hosts

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -130,3 +130,7 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = [
+    "web-production-f136.up.railway.app",
+]
